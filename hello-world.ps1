a3832db2b9a8d04c8729cc2c56b2f2e08a5b76ba
gitoldi <rijsbergenm@gmail.com>
2018-05-01 19:27:04 +0200

    This is a first PS1 example for this project.

---
+++ hello-world.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+    This is a PowerShell version of Hello World.
+
+.DESCRIPTION
+    This is a longer description about the PowerShell 'Hello World' example.
+    Let's get some starting stuff going like this help information.
+    The help is added via the 'comment-help' option.
+
+.EXAMPLE
+    PS C:\> hello-world.ps1
+    Hello there
+
+.INPUTS
+    Inputs:
+        - At first creation no input.
+
+.OUTPUTS
+    Output:
+        - At first creation, just 'Hello there'.
+
+.NOTES
+    Author  : Marcel Rijsbergen
+    Created : 180501
+    History :
+        - 180501 MR, Created.
+
+#>
+Write-Host 'Hello there'
